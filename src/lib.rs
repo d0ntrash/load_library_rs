eaab10b60a11287df576d0c9f3e47dbd6357fc89
d0ntrash <buecheko@protonmail.com>
2022-08-13 10:40:24 +0200

    setting permissions of sections

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use windows_sys::Win32::System::Memory::HeapHandle;
 use windows_sys::Win32::System::Memory::HEAP_FLAGS;
 use windows_sys::Win32::System::SystemServices::IMAGE_DOS_HEADER;
 use windows_sys::Win32::System::SystemServices::IMAGE_IMPORT_DESCRIPTOR;
+use windows_sys::Win32::System::SystemServices::IMAGE_TLS_DIRECTORY64;
+use windows_sys::Win32::System::WindowsProgramming::IMAGE_DELAYLOAD_DESCRIPTOR;
 use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64;
 use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_SECTION_HEADER;
 use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_DATA_DIRECTORY;
@@ -15,8 +17,12 @@ pub type PathFindFileNameW = unsafe extern "system" fn (i32) -> i32;
 pub type CreateFileW = unsafe extern "system" fn (isize, u32, u32, isize, u32, u32, isize) -> isize;
 pub type GetFileSize = unsafe extern "system" fn (isize, isize) -> u32;
 pub type VirtualAlloc = unsafe extern "system" fn (isize, usize, u32, u32) -> isize;
+pub type VirtualProtect = unsafe extern "system" fn (isize, usize, u32, isize) -> i32;
 pub type ReadFile = unsafe extern "system" fn (isize, isize, u32, isize, isize) -> isize;
 pub type LoadLibraryA = unsafe extern "system" fn (isize) -> i32;
+pub type FlushInstructionCache = unsafe extern "system" fn (isize, isize, u32) -> i32;
+
+pub type PIMAGE_TLS_CALLBACK = unsafe extern "system" fn(isize, u32, isize);
 
 pub const GENERIC_READ: u32 = 2147483648u32;
 pub const FILE_SHARE_READ: u32 = 1u32;
@@ -27,6 +33,20 @@ pub const MEM_COMMIT: u32 = 4096u32;
 pub const MEM_RESERVE: u32 = 8192u32;
 pub const PAGE_READWRITE: u32 = 4u32;
 pub const IMAGE_DIRECTORY_ENTRY_IMPORT: u32 = 1;
+pub const IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT: u32 = 13u32;
+pub const IMAGE_DIRECTORY_ENTRY_TLS: u32 = 9u32;
+pub const IMAGE_SCN_MEM_EXECUTE: u32 = 536870912u32;
+pub const IMAGE_SCN_MEM_READ: u32 = 1073741824u32;
+pub const IMAGE_SCN_MEM_WRITE: u32 = 2147483648u32;
+pub const PAGE_NOACCESS: u32 = 1u32;
+pub const PAGE_WRITECOPY: u32 = 8u32;
+pub const PAGE_READONLY: u32 = 2u32;
+pub const PAGE_EXECUTE: u32 = 16u32;
+pub const PAGE_EXECUTE_WRITECOPY: u32 = 128u32;
+pub const PAGE_EXECUTE_READ: u32 = 32u32;
+pub const PAGE_EXECUTE_READWRITE: u32 = 64u32;
+pub const IMAGE_SCN_MEM_NOT_CACHED: u32 = 67108864u32;
+pub const PAGE_NOCACHE: u32 = 512u32;
 
 // https://internals.rust-lang.org/t/get-the-offset-of-a-field-from-the-base-of-a-struct/14163
 macro_rules! get_offset {
@@ -38,6 +58,36 @@ macro_rules! get_offset {
 
         member_ptr as usize - dummy_ptr as usize
     })
+}
+
+fn get_section_protection(characteristics: u32) -> u32 {
+    let executable = ((characteristics & IMAGE_SCN_MEM_EXECUTE) != 0) | true;
+    let readable = ((characteristics & IMAGE_SCN_MEM_READ) != 0) | true;
+    let writeable = ((characteristics & IMAGE_SCN_MEM_WRITE) != 0) | true;
+
+    let mut protection = if !executable && !readable && !writeable {
+        PAGE_NOACCESS
+    } else if !executable && !readable && writeable {
+        PAGE_WRITECOPY
+    } else if !executable && readable && !writeable {
+        PAGE_READONLY
+    } else if !executable && readable && writeable {
+        PAGE_READWRITE
+    } else if executable && !readable && !writeable {
+        PAGE_EXECUTE
+    } else if executable && !readable && writeable {
+        PAGE_EXECUTE_WRITECOPY
+    } else if executable && readable && !writeable {
+        PAGE_EXECUTE_READ
+    } else {
+        PAGE_EXECUTE_READWRITE
+    };
+
+    if (characteristics & IMAGE_SCN_MEM_NOT_CACHED) != 0 {
+        protection |= PAGE_NOCACHE;
+    }
+
+    protection
 }
 
 pub fn load_library(module_name: &str) {
@@ -57,8 +107,14 @@ pub fn load_library(module_name: &str) {
     let p_virtual_alloc: VirtualAlloc = unsafe{
         std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "VirtualAlloc"))
     };
+    let p_virtual_protect: VirtualProtect = unsafe{
+        std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "VirtualAlloc"))
+    };
     let p_read_file: ReadFile = unsafe{
         std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "ReadFile"))
+    };
+    let p_flush_instruction_cache: FlushInstructionCache = unsafe{
+        std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "FlushInstructionCache"))
     };
     // TODO: Remove and use custom load_library recursive
     let p_load_library: LoadLibraryA = unsafe{
@@ -164,18 +220,13 @@ pub fn load_library(module_name: &str) {
         image_section_header = unsafe{*p_image_section_header};
     }
 
-    // Reset section header to firt entry
-    p_image_section_header = (p_image_nt_headers as usize +
-                              get_offset!(IMAGE_NT_HEADERS64, OptionalHeader) as usize +
-                              image_nt_headers.FileHeader.SizeOfOptionalHeader as usize) as *const IMAGE_SECTION_HEADER;
-    image_section_header = unsafe{*p_image_section_header};
-
 
     // TODO: Relocation
 
     // Set ImageBase to loaded module base
     image_nt_headers.OptionalHeader.ImageBase = module_base as u64;
 
+    // Resolve entry imports
     let data_directory: IMAGE_DATA_DIRECTORY = image_nt_headers.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT as usize];
 
     if data_directory.Size != 0 {
@@ -202,9 +253,53 @@ pub fn load_library(module_name: &str) {
         }
     }
 
-    for i in 0..image_nt_headers.FileHeader.NumberOfSections {
-        if image_section_header.SizeOfRawData != 0 {
+    // Resolve entry delay imports
+    let data_directory: IMAGE_DATA_DIRECTORY = image_nt_headers.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT as usize];
+    println!("{}", data_directory.Size);
+    if data_directory.Size != 0 {
+        // reset import descriptor
+        let mut p_image_import_descriptor = (module_base + data_directory.VirtualAddress as isize)
+            as *const IMAGE_DELAYLOAD_DESCRIPTOR;
+        let mut image_import_decriptor = unsafe{*p_image_import_descriptor};
 
+        loop {
+            if image_import_decriptor.DllNameRVA == 0 {
+                break
+            }
+            println!("{:x}", module_base + image_import_decriptor.DllNameRVA as isize);
+            // TODO: Check if library is present
+            //       Use LoadLibrary recursive
+            //       Delayed import helper
+            let load_status = unsafe{p_load_library(module_base + image_import_decriptor.DllNameRVA as isize)};
+
+            if load_status == 0 {
+                let os_error = Error::last_os_error();
+                println!("Failed reading dll file: {os_error:?}");
+            }
+
+            p_image_import_descriptor = (p_image_import_descriptor as usize + std::mem::size_of::<IMAGE_DELAYLOAD_DESCRIPTOR>() as usize) as *const IMAGE_DELAYLOAD_DESCRIPTOR;
+            image_import_decriptor = unsafe{*p_image_import_descriptor};
+        }
+    }
+
+    // Set protections for each section
+    // Reset section header to firt entry
+    p_image_section_header = (p_image_nt_headers as usize +
+                              get_offset!(IMAGE_NT_HEADERS64, OptionalHeader) as usize +
+                              image_nt_headers.FileHeader.SizeOfOptionalHeader as usize) as *const IMAGE_SECTION_HEADER;
+    image_section_header = unsafe{*p_image_section_header};
+
+    for _ in 0..image_nt_headers.FileHeader.NumberOfSections {
+        if image_section_header.SizeOfRawData != 0 {
+            let characteristics = image_section_header.Characteristics;
+            let mut protection = get_section_protection(characteristics);
+            unsafe{
+                p_virtual_protect(
+                    module_base + image_section_header.VirtualAddress as isize,
+                    image_section_header.SizeOfRawData as usize,
+                    protection,
+                    (&mut protection as *mut u32) as isize);
+            }
         }
         // increment pointer to the next entry
         p_image_section_header = (p_image_section_header as usize +
@@ -212,19 +307,7 @@ pub fn load_library(module_name: &str) {
                                   image_nt_headers.FileHeader.SizeOfOptionalHeader as usize) as *const IMAGE_SECTION_HEADER;
         image_section_header = unsafe{*p_image_section_header};
     }
+
+    unsafe{p_flush_instruction_cache(-1isize, 0, 0)};
+
 }
-
-    // println!("{:x}", module_base);
-    // let mut line = String::new();
-    // let _ = std::io::stdin().read_line(&mut line).unwrap();
-
-    // let dn_load_library_a: peb_walk_rs::LoadLibraryA = unsafe{
-    //     std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "LoadLibraryA"))
-    // };
-    // unsafe { dn_load_library_a("shlwapi.dll\0".as_ptr()); }
-    // let shlwapi32_base_address: HINSTANCE = peb_walk_rs::get_module_base_addr("shlwapi.dll");
-    // let p_path_find_file_name: PathFindFileNameW = unsafe{
-    //     std::mem::transmute(peb_walk_rs::get_proc_addr(shlwapi32_base_address, "PathFindFileNameW"))
-    // };
-    // let p_path = unsafe { p_path_find_file_name("C:\\Windows\\System32\\user32.dll".as_ptr() as i32)};
-    // println!("pPath: {:x}", p_path);
