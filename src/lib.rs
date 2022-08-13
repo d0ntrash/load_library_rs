080aae316bec47059fd494ef425a96fbe5d6bf57
d0ntrash <buecheko@protonmail.com>
2022-08-13 18:35:49 +0200

    push current (broken) state

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use windows_sys::Win32::System::Memory::HeapHandle;
 use windows_sys::Win32::System::Memory::HEAP_FLAGS;
 use windows_sys::Win32::System::SystemServices::IMAGE_DOS_HEADER;
 use windows_sys::Win32::System::SystemServices::IMAGE_IMPORT_DESCRIPTOR;
-use windows_sys::Win32::System::SystemServices::IMAGE_TLS_DIRECTORY64;
+use windows_sys::Win32::System::WindowsProgramming::IMAGE_THUNK_DATA64;
 use windows_sys::Win32::System::WindowsProgramming::IMAGE_DELAYLOAD_DESCRIPTOR;
 use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_NT_HEADERS64;
 use windows_sys::Win32::System::Diagnostics::Debug::IMAGE_SECTION_HEADER;
@@ -17,12 +17,14 @@ pub type PathFindFileNameW = unsafe extern "system" fn (i32) -> i32;
 pub type CreateFileW = unsafe extern "system" fn (isize, u32, u32, isize, u32, u32, isize) -> isize;
 pub type GetFileSize = unsafe extern "system" fn (isize, isize) -> u32;
 pub type VirtualAlloc = unsafe extern "system" fn (isize, usize, u32, u32) -> isize;
-pub type VirtualProtect = unsafe extern "system" fn (isize, usize, u32, isize) -> i32;
+pub type VirtualProtect = unsafe extern "system" fn (isize, usize, u32, *mut u32) -> i32;
 pub type ReadFile = unsafe extern "system" fn (isize, isize, u32, isize, isize) -> isize;
-pub type LoadLibraryA = unsafe extern "system" fn (isize) -> i32;
+pub type LoadLibraryA = unsafe extern "system" fn (isize) -> isize;
 pub type FlushInstructionCache = unsafe extern "system" fn (isize, isize, u32) -> i32;
+pub type DLLMAIN = unsafe extern "system" fn (isize, u32, isize) -> i32;
 
-pub type PIMAGE_TLS_CALLBACK = unsafe extern "system" fn(isize, u32, isize);
+//pub type PIMAGE_TLS_CALLBACK = unsafe extern "system" fn(isize, u32, isize);
+pub type PIMAGE_TLS_CALLBACK = Option<unsafe extern "system" fn(dllhandle: isize, reason: u32, reserved: isize)>;
 
 pub const GENERIC_READ: u32 = 2147483648u32;
 pub const FILE_SHARE_READ: u32 = 1u32;
@@ -31,7 +33,6 @@ pub const OPEN_EXISTING: u32 = 3u32;
 pub const INVALID_HANDLE_VALUE: isize = -1i32 as isize;
 pub const MEM_COMMIT: u32 = 4096u32;
 pub const MEM_RESERVE: u32 = 8192u32;
-pub const PAGE_READWRITE: u32 = 4u32;
 pub const IMAGE_DIRECTORY_ENTRY_IMPORT: u32 = 1;
 pub const IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT: u32 = 13u32;
 pub const IMAGE_DIRECTORY_ENTRY_TLS: u32 = 9u32;
@@ -41,12 +42,14 @@ pub const IMAGE_SCN_MEM_WRITE: u32 = 2147483648u32;
 pub const PAGE_NOACCESS: u32 = 1u32;
 pub const PAGE_WRITECOPY: u32 = 8u32;
 pub const PAGE_READONLY: u32 = 2u32;
+pub const PAGE_READWRITE: u32 = 4u32;
 pub const PAGE_EXECUTE: u32 = 16u32;
 pub const PAGE_EXECUTE_WRITECOPY: u32 = 128u32;
 pub const PAGE_EXECUTE_READ: u32 = 32u32;
 pub const PAGE_EXECUTE_READWRITE: u32 = 64u32;
 pub const IMAGE_SCN_MEM_NOT_CACHED: u32 = 67108864u32;
 pub const PAGE_NOCACHE: u32 = 512u32;
+pub const DLL_PROCESS_ATTACH: u32 = 1u32;
 
 // https://internals.rust-lang.org/t/get-the-offset-of-a-field-from-the-base-of-a-struct/14163
 macro_rules! get_offset {
@@ -61,9 +64,10 @@ macro_rules! get_offset {
 }
 
 fn get_section_protection(characteristics: u32) -> u32 {
-    let executable = ((characteristics & IMAGE_SCN_MEM_EXECUTE) != 0) | true;
-    let readable = ((characteristics & IMAGE_SCN_MEM_READ) != 0) | true;
-    let writeable = ((characteristics & IMAGE_SCN_MEM_WRITE) != 0) | true;
+    let executable = if (characteristics & IMAGE_SCN_MEM_EXECUTE) != 0 {true} else {false};
+    let readable = if (characteristics & IMAGE_SCN_MEM_READ) != 0 {true} else {false};
+    let writeable = if (characteristics & IMAGE_SCN_MEM_WRITE) != 0 {true} else {false};
+
 
     let mut protection = if !executable && !readable && !writeable {
         PAGE_NOACCESS
@@ -83,9 +87,9 @@ fn get_section_protection(characteristics: u32) -> u32 {
         PAGE_EXECUTE_READWRITE
     };
 
-    if (characteristics & IMAGE_SCN_MEM_NOT_CACHED) != 0 {
-        protection |= PAGE_NOCACHE;
-    }
+    // if (characteristics & IMAGE_SCN_MEM_NOT_CACHED) != 0 {
+    //     protection |= PAGE_NOCACHE;
+    // }
 
     protection
 }
@@ -108,7 +112,7 @@ pub fn load_library(module_name: &str) {
         std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "VirtualAlloc"))
     };
     let p_virtual_protect: VirtualProtect = unsafe{
-        std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "VirtualAlloc"))
+        std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "VirtualProtect"))
     };
     let p_read_file: ReadFile = unsafe{
         std::mem::transmute(peb_walk_rs::get_proc_addr(kernel32_base_address, "ReadFile"))
@@ -226,6 +230,7 @@ pub fn load_library(module_name: &str) {
     // Set ImageBase to loaded module base
     image_nt_headers.OptionalHeader.ImageBase = module_base as u64;
 
+    // TODO: Resolve local imports
     // Resolve entry imports
     let data_directory: IMAGE_DATA_DIRECTORY = image_nt_headers.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT as usize];
 
@@ -241,12 +246,14 @@ pub fn load_library(module_name: &str) {
             // TODO: Check if library is present
             //       Use LoadLibrary recursive
             //       Delayed import helper
-            let load_status = unsafe{p_load_library(module_base + image_import_decriptor.Name as isize)};
+            let library_handle = unsafe{p_load_library(module_base + image_import_decriptor.Name as isize)};
 
-            if load_status == 0 {
+            if library_handle == 0 {
                 let os_error = Error::last_os_error();
                 println!("Failed reading dll file: {os_error:?}");
             }
+
+            //let p_first_thunk = (module_base + )
 
             p_image_import_descriptor = (p_image_import_descriptor as usize + std::mem::size_of::<IMAGE_IMPORT_DESCRIPTOR>() as usize) as *const IMAGE_IMPORT_DESCRIPTOR;
             image_import_decriptor = unsafe{*p_image_import_descriptor};
@@ -255,7 +262,6 @@ pub fn load_library(module_name: &str) {
 
     // Resolve entry delay imports
     let data_directory: IMAGE_DATA_DIRECTORY = image_nt_headers.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT as usize];
-    println!("{}", data_directory.Size);
     if data_directory.Size != 0 {
         // reset import descriptor
         let mut p_image_import_descriptor = (module_base + data_directory.VirtualAddress as isize)
@@ -266,7 +272,6 @@ pub fn load_library(module_name: &str) {
             if image_import_decriptor.DllNameRVA == 0 {
                 break
             }
-            println!("{:x}", module_base + image_import_decriptor.DllNameRVA as isize);
             // TODO: Check if library is present
             //       Use LoadLibrary recursive
             //       Delayed import helper
@@ -298,16 +303,39 @@ pub fn load_library(module_name: &str) {
                     module_base + image_section_header.VirtualAddress as isize,
                     image_section_header.SizeOfRawData as usize,
                     protection,
-                    (&mut protection as *mut u32) as isize);
+                    &mut protection
+                );
             }
         }
         // increment pointer to the next entry
-        p_image_section_header = (p_image_section_header as usize +
-                                  get_offset!(IMAGE_NT_HEADERS64, OptionalHeader) as usize +
-                                  image_nt_headers.FileHeader.SizeOfOptionalHeader as usize) as *const IMAGE_SECTION_HEADER;
+        p_image_section_header = (p_image_section_header as isize +
+                                  (std::mem::size_of::<IMAGE_SECTION_HEADER>() as isize)) as *const IMAGE_SECTION_HEADER;
         image_section_header = unsafe{*p_image_section_header};
     }
 
     unsafe{p_flush_instruction_cache(-1isize, 0, 0)};
 
+    //     // Execute TLS callbacks
+    //     // TODO: Fix TLS callback
+    //     let data_directory: IMAGE_DATA_DIRECTORY = image_nt_headers.OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT as usize];
+
+    //     if data_directory.Size != 0 {
+    //         println!("Size: {}", data_directory.Size);
+    //         let p_image_tls_directory: *const IMAGE_TLS_DIRECTORY32 = (module_base + data_directory.VirtualAddress as isize) as *const IMAGE_TLS_DIRECTORY32;
+    //         let image_tls_directory = unsafe{*p_image_tls_directory};
+    //         let tls_callbacks = image_tls_directory.AddressOfCallBacks;
+    //         let p_tls_callback_function: PIMAGE_TLS_CALLBACK = unsafe{std::mem::transmute(module_base + tls_callbacks as isize)};
+    //         let tls_callback_function = p_tls_callback_function.unwrap();
+    //         let mut line = String::new();
+    //         let _ = std::io::stdin().read_line(&mut line).unwrap();
+    //         unsafe{tls_callback_function(module_base, DLL_PROCESS_ATTACH, 0);}
+    //     }
+
+    // TODO: Check if entry point exists
+    let dll_main: DLLMAIN = unsafe{std::mem::transmute(module_base + image_nt_headers.OptionalHeader.AddressOfEntryPoint as isize)};
+    println!("{:x}", image_nt_headers.OptionalHeader.AddressOfEntryPoint);
+    println!("{:x}", dll_main as isize);
+    let mut line = String::new();
+    let _ = std::io::stdin().read_line(&mut line).unwrap();
+    unsafe{dll_main(module_base, DLL_PROCESS_ATTACH, 0)};
 }
